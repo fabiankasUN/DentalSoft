5d2f47700252333df630448399e936c690d47ebe
fabianUN <faarodriguezbe@unal.edu.co>
2017-05-10 22:18:36 -0500

    Add project files.

---
+++ DentalSoft/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DentalSoft.Startup))]
+namespace DentalSoft
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
